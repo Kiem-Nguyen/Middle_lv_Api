8c8a3ba13e094cfb7b9d0c7397ffb27414d34dd0
Kiem Nguyen <vankiem95@gmail.com>
2019-09-26 15:20:06 +0700

    add more

---
+++ FoodFunday/Models/MenuTitleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodFunday.Models
+{
+    public class MenuTitleViewModel
+    {
+        public string Name { get; set; }
+        public string icon { get; set; }
+    }
+}
